bd9566d0749ed684b6f416f42914119bc8c9388a
2142692 <2142692@cmontmorency.qc.ca>
2023-04-14 13:34:19 -0400

    yep

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
-const anim = gsap.fromTo('.flech', 
+const anim = 
+gsap.fromTo('.flech', 
 
-{ x: '0%'}, 
-{ repeat: -1,
-x: '100vw',
-duration: 10}
+  { x: '0%'}, 
+  { repeat: -1,
+  x: '5vw',
+  duration: 1}
 );

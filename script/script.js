e7712cb9bc8ed7d5a576df91d9e89e2b8be5464b
2142692 <89608255+2142692@users.noreply.github.com>
2023-04-15 18:19:26 -0400

    marcher

--- script/script.js
+++ script/script.js
@@ -9,19 +9,28 @@ gsap.fromTo('.flech',
   duration: 1}
 );
 
- 
+
+const marche =   gsap.fromTo('.spritesheet', 
+
+{ x: '-600px'}, 
+{  toggleActions: 'restart complete reverse reset',
+x: '1500px',
+duration: 10}
+          
+); 
+
 let body=document.querySelector("body");
 window.addEventListener("scroll", function() {
   body.classList.add('is-scrolling');
+  marche.play();
+ 
 });
 
 
 
 const minuteur = window.setInterval(function() {
-  body.classList.remove('is-scrolling');
+  body.classList.remove('is-scrolling')
+  marche.pause()
 }, 100);
-
-
-
 
 

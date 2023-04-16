a8d56300a6cde2f7d66e30e1821ef0cd6a6ceb31
2142692 <89608255+2142692@users.noreply.github.com>
2023-04-16 15:44:47 -0400

    scroll

--- script/script.js
+++ script/script.js
@@ -9,12 +9,26 @@ gsap.fromTo('.flech',
   duration: 1}
 );
 
+let header = document.querySelector("header");
+let section = document.querySelector("section");
+let ciel = document.querySelector("#chap1");
+const decor=gsap.to(ciel, {
+ y:"-100%", 
+
+  scrollTrigger: {
+    markers: true,
+    scrub: true,
+    start: 'top 0%',
+    end: 'bottom 10%',
+    toggleActions: 'restart complete reverse reset',
+    trigger: header,
+  }, });
 
 const marche =   gsap.fromTo('.spritesheet', 
 
 { x: '-600px'}, 
 {  toggleActions: 'restart complete reverse reset',
-x: '1500px',
+x: '100px',
 duration: 10}
           
 ); 
@@ -23,7 +37,7 @@ let body=document.querySelector("body");
 window.addEventListener("scroll", function() {
   body.classList.add('is-scrolling');
   marche.play();
- 
+
 });
 
 
@@ -31,6 +45,6 @@ window.addEventListener("scroll", function() {
 const minuteur = window.setInterval(function() {
   body.classList.remove('is-scrolling')
   marche.pause()
-}, 100);
 
+}, 100);
 
